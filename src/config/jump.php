7e1ade70fe8fe9f926df75b4b386019778a8e656
houjit <83946244+houjit021@users.noreply.github.com>
2021-05-10 18:15:29 +0800

    houjit021
    base

---
+++ src/config/jump.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 厚匠社区团 - 团队十年电商经验汇集巨献!
+ * =========================================================
+ * Copy right 2019-2029 上海厚匠网络科技有限公司, 保留所有权利。
+ * ----------------------------------------------
+ * 官方网址: https://www.houjit.com
+ * =========================================================
+ */
+return[
+    // 默认跳转页面对应的模板文件
+    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/liliuwei/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/liliuwei/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+];
